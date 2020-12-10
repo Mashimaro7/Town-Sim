0da7a920effcdb2564fa272ebaf8592b3c762e7d
Mashimaro7 <63817298+Mashimaro7@users.noreply.github.com>
2020-12-11 01:45:31 +0700

    0.1

---
+++ Assets/Scripts/TownsPerson.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TownsPerson : MonoBehaviour
+{
+    public float hunger = 100, thirst = 100, happiness = 50, money = 50,social = 100,energy = 100;
+    const float valueMax = 100;
+    int storeToVisitIndex;
+    float timer = 1;
+
+    public float hungerRate = 2, thirstRate = 3, energyRate = 1, socialRate = 1;
+    public TextMeshProUGUI hungerText, thirstText;
+
+    public Transform[] storeFronts;
+
+    TownspersonMovement movement;
+
+    private void Start()
+    {
+        movement = GetComponent<TownspersonMovement>();   
+    }
+
+    void Update()
+    {
+        hungerText.text = "Hunger: " + hunger.ToString("F0");
+        thirstText.text = "Thirst: " + thirst.ToString("F0");
+
+        if (hunger > 0)
+        {
+            hunger -= Time.deltaTime * 2;
+        }
+
+        else
+        {
+            hunger = 0;
+        }
+
+        if (thirst > 0)
+        {
+            thirst -= Time.deltaTime * 3;
+        }
+
+        else
+        {
+            thirst = 0;
+        }
+     
+        if(hunger < 50 && money > 10)
+        {
+            GoToStore(0);
+        }
+    }
+
+    void GoToStore(int storeType)
+    {
+        movement.ChangeState(States.HeadingToLocation);
+        movement.Move(storeFronts[storeType].position);
+                   
+    }
+}
---
+++ Assets/Scripts/TownspersonMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum States
+{
+    Wandering = 0,
+    Eating = 1,
+    Sleeping = 2,
+    Socializing = 3,
+    HeadingToLocation = 4
+}
+
+public class TownspersonMovement : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public Vector2 secBetweenWanderMinMax;
+    public float wanderRange;
+    States states;
+    float wanderTimer;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        Wander();
+        states = States.Wandering;
+    }
+
+    private void Update()
+    {
+        if(wanderTimer > 0)
+        {
+            wanderTimer -= Time.deltaTime;
+        }
+        else
+        {
+            if(states == States.Wandering)
+            Wander();
+            else
+            {
+                float secBetweenWander = Random.Range(secBetweenWanderMinMax.x, secBetweenWanderMinMax.y);
+            }
+        }
+    }
+
+    public void ChangeState(States state)
+    {
+        states = state;
+    }
+
+    void Wander()
+    {
+        float secBetweenWander = Random.Range(secBetweenWanderMinMax.x, secBetweenWanderMinMax.y);
+        float randomX = Random.Range(-wanderRange, wanderRange);
+        float randomZ = Random.Range(-wanderRange, wanderRange);
+
+        Vector3 wanderSpot = transform.position + new Vector3(randomX, 0, randomZ);
+        if (Vector3.Distance(transform.position,wanderSpot) > 0.5f)
+        {
+            Move(wanderSpot);
+        }
+
+        wanderTimer = secBetweenWander;
+
+        
+    }
+
+    public void Move(Vector3 dest)
+    {
+        nav.SetDestination(dest);
+    }
+}
