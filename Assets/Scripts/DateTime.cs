24048a52ba54175996f95ac3a9977b04b35ce20c
Mashimaro7 <63817298+Mashimaro7@users.noreply.github.com>
2021-01-10 22:02:27 +0700

    0.15

---
+++ Assets/Scripts/DateTime.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateTime : MonoBehaviour
+{
+    static DateTime _dateTime;
+    public float timeOfDay;
+    [SerializeField]
+    int maxDayTime = 1200;
+
+    int dayCount = 0;
+    Transform sunLight;
+
+    public static DateTime date
+    {
+        get
+        {
+            if (_dateTime != null)
+            {
+                return _dateTime;
+            }
+            else
+            {
+                _dateTime = new DateTime();
+            }
+            
+            return _dateTime;
+        }
+    }
+
+    void Awake()
+    {
+        _dateTime = this;
+        sunLight = GameObject.Find("Directional Light").transform;
+    }
+
+
+    private void Update()
+    {
+        sunLight.transform.rotation = Quaternion.Euler((timeOfDay * 360) - 90, 170, 0);
+        if (timeOfDay < 1)
+        {
+            timeOfDay += Time.deltaTime / maxDayTime;
+        }
+        else
+        {
+            timeOfDay = 0;
+            dayCount++;
+        }
+    }
+
+}
